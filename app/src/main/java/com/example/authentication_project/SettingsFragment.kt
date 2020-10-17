648b155ec40664a84398d1f68dddd7256608b24c
amaa <amaa@students.chalmers.se>
2020-10-18 01:54:54 +0200

    Added settings screen. Updated styling. Updated transition styles of nav actions. Added resources.

--- app/src/main/java/com/example/authentication_project/SettingsFragment.kt
+++ app/src/main/java/com/example/authentication_project/SettingsFragment.kt
@@ -12,24 +12,21 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.settings_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
-        view.findViewById<ImageButton>(R.id.settingsButton).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_SettingsFragment)
+        view.findViewById<ImageButton>(R.id.returnHome).setOnClickListener {
+            findNavController().navigate(R.id.action_SettingsFragment_to_SecondFragment)
         }
     }
 }
