aab88e3def1264466ef5ff321ea9955777c7c8f6
komhitnu <gusjlakjch@student.gu.se>
2020-10-20 12:50:32 +0200

    added request permissions to write_external_storage when the app is downloaded for the first time

--- app/src/main/java/com/example/authentication_project/MainActivity.kt
+++ app/src/main/java/com/example/authentication_project/MainActivity.kt
@@ -1,20 +1,59 @@
 package com.example.authentication_project
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.os.CountDownTimer
 import android.view.Menu
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkPermission()) {
+
+            } else {
+                requestPermission()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
+    }
+
+    // Boolean to check if permission was granted
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Request to use External Storage
+    private fun requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(this, "Write External Storage permission allows us to store your password. Please allow this permission in App Settings.",
+                Toast.LENGTH_LONG).show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), PERMISSION_REQUEST_CODE)
+        }
     }
 }
